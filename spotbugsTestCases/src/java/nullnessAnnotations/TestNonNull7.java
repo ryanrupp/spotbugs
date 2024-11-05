b84c649992260b9baa52c7b6f568f03b2efb2e5b
Ryan Rupp <3022260+ryanrupp@users.noreply.github.com>
2024-11-05 16:03:35 -0600

    Add a false positive example where multiple statements are used i.e. get nullable result, then assert it with requireNonNull (see multipleStatementsFails really shouldn't be flagged).

--- spotbugsTestCases/src/java/nullnessAnnotations/TestNonNull7.java
+++ spotbugsTestCases/src/java/nullnessAnnotations/TestNonNull7.java
@@ -1,9 +1,10 @@
 package nullnessAnnotations;
 
-import com.google.common.base.Verify;
 import com.google.common.base.Preconditions;
+import com.google.common.base.Verify;
 import org.apache.commons.lang3.Validate;
 
+import javax.annotation.CheckForNull;
 import java.io.File;
 import java.util.Objects;
 
@@ -52,5 +53,25 @@ public class TestNonNull7 {
         for (File subFile : Preconditions.checkNotNull(rootFile.listFiles())) {
             System.out.println(subFile.getName());
         }
+    }
+
+    public void multipleStatementsFails() {
+        String result = methodThatCanReturnNull();
+        result = Objects.requireNonNull(result); // NP_NULL_ON_SOME_PATH_FROM_RETURN_VALUE reported on this line
+        System.out.println(result.length());
+    }
+
+    public void nullCheckWithBranchingWorks() {
+        String result = methodThatCanReturnNull();
+        if (result == null) {
+            throw new IllegalArgumentException("This works!");
+        }
+
+        System.out.println(result.length());
+    }
+
+    @CheckForNull
+    private String methodThatCanReturnNull() {
+        return null;
     }
 }
